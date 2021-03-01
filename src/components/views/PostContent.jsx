250bf163cd93c7c13264d961f5af02807f122171
hkgnp <ben@pngs.cc>
2021-03-02 00:39:16 +0800

    Implemented external json file

--- src/components/views/PostContent.jsx
+++ src/components/views/PostContent.jsx
@@ -2,56 +2,23 @@ import React from 'react';
 import Post from './Post';
 import ManagePagination from '../general/ManagePagination';
 import Paginate from '../general/Paginate';
+import axios from 'axios';
 
 export default class PostContent extends React.Component {
   state = {
     pageSize: 3,
     currentPage: 1,
-    posts: [
-      {
-        _id: 0,
-        title: 'Title 1',
-        category: 'Category 123',
-        description: 'Description 123',
-        location: 310098,
-      },
-      {
-        _id: 1,
-        title: 'Title 2',
-        category: 'Category ABC',
-        description: 'Description ABC',
-        location: 119074,
-      },
-      {
-        _id: 2,
-        title: 'Title 3',
-        category: 'Category Smith',
-        description: 'Description Smith',
-        location: 650626,
-      },
-      {
-        _id: 3,
-        title: 'Title 4',
-        category: 'Category Smith',
-        description: 'Description Smith',
-        location: 650626,
-      },
-      {
-        _id: 4,
-        title: 'Title 5',
-        category: 'Category Smith',
-        description: 'Description Smith',
-        location: 650626,
-      },
-      {
-        _id: 5,
-        title: 'Title 6',
-        category: 'Category Smith',
-        description: 'Description Smith',
-        location: 650626,
-      },
-    ],
+    posts: [],
+    loaded: false,
   };
+
+  async componentDidMount() {
+    let response = await axios.get('posts.json');
+    this.setState({
+      posts: response.data,
+      loaded: true,
+    });
+  }
 
   renderPosts = () => {
     const allPosts = Paginate(
