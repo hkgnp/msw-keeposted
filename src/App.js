c9772173aafed9d5dda92f2a4863c474c4cc76f8
hkgnp <ben@pngs.cc>
2021-03-01 18:19:38 +0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import NavigationBar from './components/NavigationBar';
 import PostContent from './components/PostContent';
 import Footer from './components/Footer';
 import { Container, Row, Col } from 'reactstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <Container className="parentContainer" fluid={true}>
-      <Row>
-        <Col>
-          <NavigationBar />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="postContainer">
-          <PostContent />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Footer />
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Container className="parentContainer" fluid={true}>
+        <Row>
+          <Col>
+            <NavigationBar />
+          </Col>
+        </Row>
+        <Switch>
+          <Route path="/posts">
+            <Row>
+              <Col className="postContainer">
+                <PostContent />
+              </Col>
+            </Row>
+          </Route>
+        </Switch>
+        <Row>
+          <Col>
+            <Footer />
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
