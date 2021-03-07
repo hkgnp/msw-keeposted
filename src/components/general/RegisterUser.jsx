705f3441965538e5f1c7d893a20254dbc282783e
hkgnp <benpng@u.nus.edu>
2021-03-07 13:43:31 +0800

    Successfully implemented user registration form

--- src/components/general/RegisterUser.jsx
+++ src/components/general/RegisterUser.jsx
@@ -1,3 +1,18 @@
-const RegisterUser = (props) => {};
+import axios from 'axios';
+
+const RegisterUser = async (props, req, res) => {
+  const baseUrl = 'https://7000-amethyst-elk-z1e2nqxe.ws-us03.gitpod.io';
+
+  /// Send to collection 'POST DETAILS'
+  await axios({
+    method: 'post',
+    url: `${baseUrl}/user/register`,
+    data: {
+      name: props.name,
+      email: props.email,
+      password: props.password,
+    },
+  });
+};
 
 export default RegisterUser;
