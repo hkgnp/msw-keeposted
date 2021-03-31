4a7583b5ed07fe18b98caf34b80e7fae95f7e45a
hkgnp <ben@pngs.cc>
2021-03-31 14:23:51 +0800

    Implemented link on error page

--- src/components/views/ErrorPage.jsx
+++ src/components/views/ErrorPage.jsx
@@ -12,7 +12,7 @@ export default class ErrorPage extends React.Component {
         <h1>Please Bear With Us</h1>
         <p>
           There is an error retrieving the data from the database. Our engineers
-          are already fixing the issue. Please wait.
+          are already fixing the issue. Please wait a while and <a href="https://suspicious-northcutt-d2f899.netlify.app/posts">click here to try again</a>.
         </p>
       </Col>
     );
